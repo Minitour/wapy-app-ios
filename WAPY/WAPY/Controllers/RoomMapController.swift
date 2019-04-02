470dbd8f7f515fd057eef3d12e053165d2417a8b
Tony Zaitoun <tony.z.1711@gmail.com>
2019-04-03 00:18:53 +0300

    Added camera detection with field of view representation

--- WAPY/WAPY/Controllers/RoomMapController.swift
+++ WAPY/WAPY/Controllers/RoomMapController.swift
@@ -15,6 +15,7 @@ public class RoomMapController: UIViewController {
         let config = ARWorldTrackingConfiguration()
         config.isAutoFocusEnabled = true
         config.planeDetection = [.horizontal,.vertical]
+        config.environmentTexturing = .automatic
         guard let referenceObjects = ARReferenceObject.referenceObjects(inGroupNamed: "the_box", bundle: nil) else {
             fatalError("Missing expected asset catalog resources.")
         }
@@ -31,6 +32,8 @@ public class RoomMapController: UIViewController {
 
         sceneView.translatesAutoresizingMaskIntoConstraints = false
 
+        sceneView.delegate = self
+
         NSLayoutConstraint.activate([
             sceneView.topAnchor.constraint(equalTo: view.topAnchor),
             sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -39,10 +42,50 @@ public class RoomMapController: UIViewController {
         ])
 
     }
+
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        sceneView.session.run(sessionConfig)
+    }
+
+    public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        sceneView.session.pause()
+    }
 }
 
 extension RoomMapController: ARSCNViewDelegate {
 
+    public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let objectAnchor = anchor as? ARObjectAnchor {
+            let pyramid = SCNPyramid(width: 0.1, height: 0.2, length: 0.1)
+            let reflectiveMaterial = SCNMaterial()
+            reflectiveMaterial.lightingModel = .physicallyBased
+            reflectiveMaterial.metalness.contents = 1.0
+            reflectiveMaterial.roughness.contents = 0.2
+
+            pyramid.firstMaterial? = reflectiveMaterial
+            pyramid.firstMaterial?.isDoubleSided = true
+
+            //
+            guard let name = objectAnchor.referenceObject.name, name == "box" else { return }
+
+            print("detected box")
+
+            let pyramidNode = SCNNode(geometry: pyramid)
+
+            let width = objectAnchor.referenceObject.extent.z
+
+            // set position to pyramid node to be the center of the detected object.
+            pyramidNode.position = SCNVector3(objectAnchor.referenceObject.center.x,
+                                              objectAnchor.referenceObject.center.y,
+                                              objectAnchor.referenceObject.center.z + Float(pyramid.height) + width / 2.0)
+
+            pyramidNode.eulerAngles = SCNVector3(-Float.pi / 2.0, 0.0, 0.0)
+
+            node.addChildNode(pyramidNode)
+        }
+    }
 }
 
 extension RoomMapController: ARSessionDelegate {
